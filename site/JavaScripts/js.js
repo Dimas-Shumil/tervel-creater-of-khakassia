2fb3041b9400aa7e4cb77684005856f59c6325c9
Dmitry Shumilov <shum0@mail.ru>
2025-06-15 18:29:42 +0700

    main screen

--- site/JavaScripts/js.js
+++ site/JavaScripts/js.js
@@ -1,6 +1,21 @@
 // скрипт для кнопки бургер меню
-document.querySelector('.burger').addEventListener('click',function() {
+document.querySelector('.burger').addEventListener('click', function () {
   this.classList.toggle('active');
   document.querySelector('.nav').classList.toggle('open')
 })
+// кнопка
+
+// Создание кнопки
+const button = document.createElement('button');
+button.id = 'redirectButton';
+button.textContent = 'Перейти в конструктор';
+
+// Добавление обработчика
+button.addEventListener('click', () => {
+  window.location.href = 'https://example.com'; // Замените на нужный URL
+ 
+});
+
+// Вставка в DOM
+document.getElementById('buttonContainer').appendChild(button);
 
